df78741a584f2308ebdbc1af95bfbe5b77fcf9fa
titopsur <titopsur@gmail.com>
2013-10-04 15:06:26 +0200

    aa

--- hello.py
+++ hello.py
@@ -5,10 +5,27 @@ import os
 __author__ = 'Radostin'
 from flask import Flask, url_for
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+
+user = User('test', 'test@test.com')
+db.session.add(user)
+db.session.commit()
 
 @app.route("/")
 def hello():
