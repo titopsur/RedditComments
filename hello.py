179a96682b0eaa3d17c43878a32b647c9c7fd054
titopsur <titopsur@gmail.com>
2013-10-04 15:00:14 +0200

    aa

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 from werkzeug.utils import redirect
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 __author__ = 'Radostin'
 from flask import Flask, url_for
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
