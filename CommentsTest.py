5d5fe24539d3537f86735e7ff983bfa7b0c13733
titopsur <titopsur@gmail.com>
2013-10-04 11:23:25 +0200

    Initial commit

---
+++ CommentsTest.py
@@ -0,0 +1,27 @@
+__author__ = 'Radostin'
+import praw
+import operator
+
+r = praw.Reddit(user_agent='TestRComments1.0')
+
+subreddit = r.get_subreddit('programming')
+
+
+submissions = subreddit.get_hot(limit=3)
+
+html = "<html><table border='1'>"
+
+for submission in submissions:
+    html += "<tr><td colspan='3'><b>" + str(submission) + "</b></td></tr>"
+    submission.replace_more_comments(limit=None, threshold=0)
+    flat_comments = praw.helpers.flatten_tree(submission.comments)
+    sorted_comments = sorted(flat_comments, key=operator.attrgetter("score"), reverse=True)
+    sorted_comments = sorted_comments[0:3]
+    for comment in sorted_comments:
+        html += "<tr><td>" + str(comment.score) + "</td><td colspan='2'>" + comment.body + "</td></tr>"
+
+html += "</table></html>"
+
+outfile = open("test.html", "w+")
+outfile.write(html.encode('utf-8'))
+outfile.close()
